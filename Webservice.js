9fb86bcf5cff59a55d30f2d714b494a6840ccf75
Christo Greeff <christogreeff@gmail.com>
2014-07-02 18:07:51 +0200

    Rafactor and header
    Refactoring method for performance and added X-Requested-With header to
    indicate AJAX call

--- Webservice.js
+++ Webservice.js
@@ -1,47 +1,59 @@
-// =============================================
+// ==========================================================================
 // Author: Francois Laubscher
 // Date: 2014-07-02
 // Description: Webservice wrapper library
-// =============================================
+// Update: 2014-07-02, Christo Greeff - Added X-Requested-With + refactoring
+// ==========================================================================
 
+// constructor
 function WebService(url, contentType) {
     this.contentType = contentType;
     this.serviceUrl = url;
-    this.CallService = function (method, msgBody, onSuccess, onFail) {
+}
 
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open(method, this.serviceUrl, true);
+// Call Service
+// method: POST/GET
+// msgBody: POST content
+// onSuccess: success method
+// onFail: fail method
+WebService.prototype.CallService = function (method, msgBody, onSuccess, onFail) {
+    var xmlHttp = new XMLHttpRequest();
 
-        // check if a content type is specified
-        if (this.contentType != null || this.contentType != undefined) {
-            xmlHttp.setRequestHeader("Content-Type", this.contentType);
+    // fire this event whenever the service state changes
+    xmlHttp.onreadystatechange = function () {
+        switch (xmlHttp.readyState) {
+            case 0:
+                // request not initialized
+                // onFail
+                onFail("request not initialized.");
+                break;
+            case 4:
+                // request finished and response is ready
+                if (xmlHttp.status == 200) {
+                    // response from server is ok
+                    onSuccess(xmlHttp.responseText);
+
+                } else {
+                    // anything else failed
+                    onFail("Error calling service. Status code: " + xmlHttp.status);
+                }
+                break;
         }
-
-        // fire this event whenever the service state changes
-        xmlHttp.onreadystatechange = function () {
-            switch (xmlHttp.readyState) {
-                case 0:
-                    // request not initialized
-                    // onFail
-                    onFail("request not initialized.");
-                    break;
-                case 4:
-                    // request finished and response is ready
-                    if (xmlHttp.status == 200) {
-                        // response from server is ok
-                        onSuccess(xmlHttp.responseText);
-
-                    } else {
-                        // anything else failed
-                        onFail("Error calling service. Status code: " + xmlHttp.status);
-                    }
-                    break;
-            }
-        }
-
-        // finally send the request
-        xmlHttp.send(msgBody);
     }
+
+    // open the request
+    xmlHttp.open(method, this.serviceUrl, true);
+
+    // check if a content type is specified
+    if (this.contentType != null || this.contentType != undefined) {
+        xmlHttp.setRequestHeader("Content-Type", self.contentType);
+    }
+
+    // tells server that this call is made for ajax purposes
+    xmlHttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); 
+
+    // finally send the request
+    xmlHttp.send(msgBody);
 }
 
 // This function calls the web service by using HTTP GET
